d3135828157e88d51394ecd08e345d2b39f399e7
SamYoung <yangg052@gmail.com>
2018-06-11 15:36:54 +0800

    Update get_windows_spotlight_pictures.py
    creat folder , delete temp

--- get_windows_spotlight_pictures.py
+++ get_windows_spotlight_pictures.py
@@ -1,49 +1,74 @@
-# 获取 Windows 的锁屏壁纸
-
+# Script Name   :   get Windows 10 spotlight pictures
+# Author        :   yanggang
+# Created       :   2018/6/11
+        
 import os
-import sys
 import shutil
-#import time
+import sys
 from PIL import Image
 
-# 暂存目录 wallpapers_temp 及 最终存放目录 wallpapers
-save_folder_temp = dir_path = os.path.dirname(os.path.realpath(__file__)) + '\wallpapers_temp'
-save_folder = dir_path = os.path.dirname(os.path.realpath(__file__)) + '\wallpapers'
 
-# 获取锁屏壁纸存储位置
-wallpaper_folder = os.getenv('LOCALAPPDATA') + ('\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets')
+def savefolder():
+    # save folder
+    global save_folder_temp, save_folder
+    save_folder_temp = os.path.dirname(os.path.realpath(__file__)) + '\\wallpapers_temp'
+    save_folder = os.path.dirname(os.path.realpath(__file__)) + '\\wallpapers'
+    
+    if os.path.exists(save_folder_temp):
+        print('existed')
+    else:
+        os.makedirs(save_folder_temp)
 
-# 列出所有的文件
-wallpapers = os.listdir(wallpaper_folder)
+    if os.path.exists(save_folder):
+        print('existed')
+    else:
+        os.makedirs(save_folder)
 
-for wallpaper in wallpapers:
-    wallpaper_path = os.path.join(wallpaper_folder, wallpaper)
-    # 先初步筛选，貌似也没啥用
-    if (os.path.getsize(wallpaper_path) / 1024) < 150:
-        continue
-    time = os.path.getctime(wallpaper_path)
-    wallpaper_name = str(time) + '.jpg'
-    save_path = os.path.join(save_folder_temp, wallpaper_name)
-    shutil.copyfile(wallpaper_path, save_path)
-    print('save wallpaper' + save_path)
 
-# 上面提取出的壁纸有些是不符合我们的要求的，所以下面进一步筛选
+def first_filter():
+    wallpapers = os.listdir(wallpaper_folder)
+    for wallpaper in wallpapers:
+        wallpaper_path = os.path.join(wallpaper_folder, wallpaper)
+        # 先初步筛选，貌似也没啥用
+        if (os.path.getsize(wallpaper_path) / 1024) < 150:
+            continue
+        time = os.path.getctime(wallpaper_path)
+        wallpaper_name = str(time) + '.jpg'
+        save_path = os.path.join(save_folder_temp, wallpaper_name)
+        shutil.copyfile(wallpaper_path, save_path)
+        print('save wallpaper' + save_path)
 
-# 列出暂存目录下的所有文件
-wallpapers_2 = os.listdir(save_folder_temp)
 
-for wallpaper_2 in wallpapers_2:
-    wallpaper_2_path = os.path.join(save_folder_temp, wallpaper_2)
-    img = Image.open(wallpaper_2_path)
-    imgSize = img.size
-    # 筛选出 1920 * 1080 分辨率的壁纸
-    if (imgSize != (1920, 1080)):
-        continue
-    save_2_path = os.path.join(save_folder, wallpaper_2)
-    shutil.copyfile(wallpaper_2_path, save_2_path)
-    print('save_2 wallpaper' + save_2_path)
+def second_filter():
+    wallpapers_2 = os.listdir(save_folder_temp)
+    for wallpaper_2 in wallpapers_2:
+        wallpaper_2_path = os.path.join(save_folder_temp, wallpaper_2)
+        img = Image.open(wallpaper_2_path)
+        imgSize = img.size
+        # 筛选出 1920 * 1080 分辨率的壁纸
+        if (imgSize != (1920, 1080)):
+            continue
+        save_2_path = os.path.join(save_folder, wallpaper_2)
+        shutil.copyfile(wallpaper_2_path, save_2_path)
+        print('save_2 wallpaper' + save_2_path)
 
-# TODO
-# 1. 删除暂存文件
-# 2. 筛选步骤过于复杂
-# 3. 存储位置的创建
+
+def main():
+
+    savefolder()
+
+    # wallpapers folder
+    global wallpaper_folder
+    wallpaper_folder = os.getenv('LOCALAPPDATA') + ('\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets')
+
+    #first filter
+    first_filter()
+
+    # second filter
+    second_filter()
+
+    # delete temp folder
+    shutil.rmtree(save_folder_temp)
+
+if __name__ == '__main__':
+    main()
